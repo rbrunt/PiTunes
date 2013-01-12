b9d994b713e6dfc8ecc4e12619118b19ede926fe
Richard Brunt <r.brunt@gmail.com>
2013-01-12 21:22:31 +0000

    Cleaned syntax, added more comments to settings

--- settings.py
+++ settings.py
@@ -1,7 +1,21 @@
 """Settings for PiTunes. Put here to be easily acessable"""
+
+################
+# MPD Settings #
+################
+
 HOSTNAME = "localhost"
 MPD_PORT = 6600
+
+#######################
+# Web Server Settings #
+#######################
+
 PORT = 8080 # The port for the web server to listen on
-DEBUG=True # Print debug messages to terminal?
+DEBUG = True # Print debug messages to terminal?
+
+####################
+# Misc Preferences #
+####################
 
 FADE_ON_PLAY_PAUSE = False # Do you want to fade the music when pausing / resuming? Works better on computers than the RPi...
