fe0538e7465e5a791838579fa5e679d6dbdeaf7b
Richard Brunt <r.brunt@gmail.com>
2013-01-12 21:16:44 +0000

    Added option to stop fading on play pause
    fading doesn't work very well on RPi... (probably a hardware limitation)

--- settings.py
+++ settings.py
@@ -3,3 +3,5 @@ HOSTNAME = "localhost"
 MPD_PORT = 6600
 PORT = 8080 # The port for the web server to listen on
 DEBUG=True # Print debug messages to terminal?
+
+FADE_ON_PLAY_PAUSE = False # Do you want to fade the music when pausing / resuming? Works better on computers than the RPi...
