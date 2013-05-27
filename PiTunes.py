9cfe3aa38010ffef8d0b6c5d21c30e7f74a65c4f
Richard Brunt <r.brunt@gmail.com>
2013-05-27 01:10:31 +0100

    Tidied up main python file, moved upload endpoint

--- PiTunes.py
+++ PiTunes.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 #########################################################################
 #	This file is part of PiTunes.
 #	
@@ -56,6 +55,7 @@ def fadeout(timeout=0.3):
 	else:
 		client.pause()
 	if settings.DEBUG: print "paused"
+
 def fadein(timeout=0.4):
 	"""Fades in the music, restoring it to volume that it was at when it was paused.
 
@@ -101,7 +101,6 @@ def search(term,tag="any"):
 	
 	return response
 
-
 def getalbumart():
 #	file = File("/media/Win7/Users/Richard/Music/%s" % client.currentsong()['file'])
 #	print file
@@ -109,7 +108,6 @@ def getalbumart():
 #	with open('artwork.jpg','wb') as img:
 #		img.write(artwork)
 	return
-
 
 class now_playing(tornado.web.RequestHandler):
 	def get(self):
@@ -126,6 +124,7 @@ class get_volume(tornado.web.RequestHandler):
 	def get(self):
 		response = json.dumps({"player":{"volume":client.status()['volume']}})
 		self.write(response)
+
 class player_status(tornado.web.RequestHandler):
 	def get(self):
 		response = json.JSONEncoder().encode(client.status())
@@ -151,7 +150,6 @@ class searchhandler(tornado.web.RequestHandler):
 	def get(self,term,tag):
 		self.write(json.JSONEncoder().encode(search(term,"album")))
 
-
 class playpause(tornado.web.RequestHandler):
 	def get(self):
 		status = client.status()['state']
@@ -162,8 +160,7 @@ class playpause(tornado.web.RequestHandler):
 		elif status == "play":
 			fadeout()
 
-
-class XHR_UploadHandler(tornado.web.RequestHandler):
+class UploadHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.redirect("/")
 	def post(self):
@@ -172,16 +169,15 @@ class XHR_UploadHandler(tornado.web.RequestHandler):
 
 				if settings.DEBUG: print "Receiving Upload..."
 				data = self.request.body
-				if settings.DEBUG: print self.request.headers
 				if not data:
 					self.write("Payload expected.")
 					self.set_status(500)
 					return
 				try:
-					#output_file = open(settings.UPLOAD_PATH + filename + uuid.uuid4() + extension, 'w') # Add a random uuid to end of filename to stop there being duplicates.
 					output_file = open(settings.UPLOAD_PATH + urllib.unquote(self.request.headers.get("X-File-Name")), 'w')
 					output_file.write(data)
 					output_file.close()
+					if settings.DEBUG: print "%s successfully uploaded" % (urllib.unquote(self.request.headers.get("X-File-Name")))
 				except:
 					if settings.DEBUG: print "there was a problem saving the file"
 					response["error"] = "There was an error saving the file to disk"
@@ -193,59 +189,6 @@ class XHR_UploadHandler(tornado.web.RequestHandler):
 				self.write(json.JSONEncoder().encode(response))
 		else:
 			self.write("Uploads have been disabled. Click <a href=\"/\">here</a> to go back home");
-
-
-
-
-class uploadHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.redirect("/")
-
-	def post(self):
-		if settings.ALLOW_UPLOADS:
-			response = {"success":False} # initalise the response dict (turned into JSON later)
-			
-			if settings.DEBUG: print "Receiving Upload..."
-			fileinfo = self.request.files["songupload"]
-			for fileinfo1 in fileinfo:
-				filename = fileinfo1["filename"]
-		#		extension = filename.split()[-1]
-		#		if extension not in settings.ALLOWED_EXTENSIONS:
-		#			validextension = False
-		#			response["error"] = "The file you uploaded wasn't one of the accepted music files. they must have one of these extensions: ", settings.ALLOWED_EXTENSIONS
-		#		else:
-		#			validextension = True
-				if settings.DEBUG: print "Someone uploaded %s" % (filename)
-				try:
-					#output_file = open(settings.UPLOAD_PATH + filename + uuid.uuid4() + extension, 'w') # Add a random uuid to end of filename to stop there being duplicates.
-					output_file = open(settings.UPLOAD_PATH + filename, 'w')
-					output_file.write(fileinfo1['body'])
-					output_file.close()
-				except:
-					if settings.DEBUG: print "there was a problem saving the file %s" % fileinfo1["filename"]
-					response["error"] = "There was an error saving the file to disk"
-		#		fileinfo = self.request.files[
-				#file1 = self.request.files
-		#		print file1
-		#		file1 = self.request.files['file1'][0]
-		#	        original_fname = file1['filename']
-		#	        extension = os.path.splitext(original_fname)[1]
-		#	        fname = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
-		#	        final_filename= fname+extension
-		#	        output_file = open("uploadedfiles/" + final_filename, 'w')
-		#	        output_file.write(file1['body'])
-		#	        self.finish("file" + final_filename + " is uploaded")
-
-
-		    	if "error" in response:
-		    		response["success"] = False
-		    	else:
-		    		response["success"] = True
-		        self.write(json.JSONEncoder().encode(response))
-		else:
-		   	self.write("Uploads have been disabled. Click <a href=\"/\">here</a> to go back home");
-
-
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -273,8 +216,7 @@ application = tornado.web.Application(
 	(r"/api/previous", prevhandler),
 	(r"/api/search/(.*)(/[a-z]{1,6})?", searchhandler),
 #	(r"/api/seek/([0-9]{1,3})",seekhandler),
-#	(r"/upload", uploadHandler),
-	(r"/XHR_Upload", XHR_UploadHandler),
+	(r"/upload", UploadHandler),
 	(r"/(.*)", MainHandler)
 ], **tornadosettings)
 
